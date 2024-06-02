e5566699dc921007ebf637782aa8f2d95fd62fee
FrancescoMonaco <76154916+FrancescoMonaco@users.noreply.github.com>
2024-06-02 14:38:46 +0200

    Update extended_test.py

--- source/extended_test.py
+++ source/extended_test.py
@@ -1,7 +1,7 @@
 from RP_MH import pmotif_find2
 from RP_DC import pmotif_find3
-import time, sys, pandas as pd, numpy as np, queue
-sys.path.append('external_dependecies')
+import time, sys, os, pandas as pd, numpy as np, queue
+sys.path.append(os.path.join(os.path.dirname(__file__), 'external_dependencies'))
 from data_loader import convert_tsf_to_dataframe
 from base import z_normalized_euclidean_distance
 from find_bin_width import find_width_discr
