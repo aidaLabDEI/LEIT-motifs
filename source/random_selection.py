5155824975b3cce347598350c03fb429c24d1f57
francescopio.monaco@studenti.unipd.it <francescopio.monaco@studenti.unipd.it>
2025-03-31 16:33:49 +0200

    Refactor rp baseline with numba

--- source/random_selection.py
+++ source/random_selection.py
@@ -1,11 +1,36 @@
 from multiprocessing import cpu_count, shared_memory, Pool
 import random
 import numpy as np
-from numpy.dtypes import StringDType
 from pyts.approximation import SymbolicAggregateApproximation, PiecewiseAggregateApproximation
 from itertools import combinations
-from scipy.sparse import dok_array
 from base import z_normalized_euclidean_distanceg
+from numba import njit, prange
+
+@njit(fastmath=True, parallel=True, cache=True)
+def fill_matrix(collision_matrix, dimensions_sets, sax_results, n, window):
+    for set_idx in prange(len(dimensions_sets)): # Loop over sets
+        set_dims = dimensions_sets[set_idx] 
+        
+        words = sax_results[:, set_dims[0]].copy() # Get first dim
+        for i in range(1, len(set_dims)):
+             words += sax_results[:, set_dims[i]] 
+             
+        sorting = np.argsort(words)
+        for idx1_enum in range(len(sorting) - 1):
+            elem1 = sorting[idx1_enum]
+            for idx2_enum in range(idx1_enum + 1, len(sorting)):
+                elem2 = sorting[idx2_enum]
+                
+                if words[elem1] == words[elem2]:
+                    # Ensure lower index is first for upper triangle matrix
+                    row, col = (elem1, elem2) if elem1 < elem2 else (elem2, elem1)
+                    # Avoid trivial matches within window
+                    if col - row >= window: # Check non-trivial match 
+                         collision_matrix[row, col] += 1
+                else:
+                    # Since sorted, no further matches for elem1
+                    break 
+    return collision_matrix
 
 def SAX(ts, chunk, transformers, window, c):
     """
@@ -15,15 +40,15 @@ def SAX(ts, chunk, transformers, window, c):
     n_samples = chunk.shape[0]
     n_dims = len(transformers)
     # Preallocate output array
-    transformed_chunk = np.empty((n_samples-window+1, n_dims), dtype=StringDType())
+    transformed_chunk = np.empty((n_samples-window+1, n_dims), dtype=np.int64())
     paa = PiecewiseAggregateApproximation(window_size=c)
     for i, transformer in enumerate(transformers):
         subsequences = [ts[s:s+window,i] for s in range(n_samples-window+1)]
         subsequences = paa.transform(subsequences)
         saxed = transformer.fit_transform(subsequences)
-        # Join into one string for each dimension of a subsequence
+        # Join into one concateneted number
         for s, elem in enumerate(saxed):
-            transformed_chunk[s, i] = str(''.join(elem))            
+            transformed_chunk[s, i] = int("".join(map(str, elem)))          
     return transformed_chunk
 
 def RP(time_series_name, n, dimensionality, window, motif_dimensionality, k_motifs, dictionary_dim, c_dimensionality, max_iter, threshold):
@@ -39,6 +64,7 @@ def RP(time_series_name, n, dimensionality, window, motif_dimensionality, k_moti
         transformer = SymbolicAggregateApproximation(
             n_bins = dictionary_dim,
             strategy="normal",
+            alphabet="ordinal",
         )
         transformers.append(transformer)
     # Divide the time series into chunks and apply SAX
@@ -49,43 +75,23 @@ def RP(time_series_name, n, dimensionality, window, motif_dimensionality, k_moti
     with Pool(cpu_count()) as pool:
         sax_results = pool.starmap(SAX, [(chunk) for chunk in chunks])
         sax_results = np.concatenate(sax_results, axis=0)
-        pass
+        sax_results = sax_results[:n-window+1, :] # Ensure correct size
+
     
     # Initialize the collision matrix and create the random projection sets 
-    collision_matrix =  dok_array(((n-window+1, n-window+1)), dtype=np.int16) #np.zeros((n-window+1, n-window+1))
+    collision_matrix = np.zeros((n-window+1, n-window+1)) #dok_array(((n-window+1, n-window+1)), dtype=np.int16) #np.zeros((n-window+1, n-window+1))
     dimensions_sets = list(combinations(np.arange(dimensionality), motif_dimensionality))
     random.shuffle(dimensions_sets)
     
     # Fill the collision matrix 
-    for set in dimensions_sets:
-        words = sax_results[:,set[0]]
-        # Concatenate the surviving dimensions into one word
-        for dimension in set[1:]:
-            np.add(words, sax_results[:,dimension], words)
-            
-        # Order lexicographycally the vector, keep an array with the original indices
-        sorting = np.argsort(words)
-        # Scan the vector, if the words match then increase the corresponding matrix entry
-        for index1, elem1 in enumerate(sorting):
-            for elem2 in sorting[index1+1:]:
-                if np.equal(words[elem1], words[elem2]):
-                    if elem1 > elem2:
-                        elem1, elem2 = elem2, elem1
-                    collision_matrix[elem1,elem2] +=1
-                else:
-                    break
-    # Zero out the diagonal and the window around it
-    for i in range(n-window+1):
-        collision_matrix[i,i] = 0
-        for j in range(i-window+1, i+window):
-            if j >= 0 and j < n-window+1:
-                collision_matrix[i,j] = 0
+    collision_matrix = fill_matrix(collision_matrix, dimensions_sets, sax_results, n, window)
     # Find the maximal entries in the collision matrix and compute the distances for them
     top = []
-    max_elements = sorted(collision_matrix.items(), key=lambda x: x[1], reverse=True)
-    dist_comp= 0
+    dist_comp = 0
+    # Take all non-zero elements in the collision matrix
+    max_elements = np.argwhere(collision_matrix > 0)
     for maximal in max_elements:
-        maximal_sub1, maximal_sub2 = maximal[0]
+        maximal_sub1, maximal_sub2 = maximal
         dist_comp += 1
         curr_dist, dim, _ = (
         z_normalized_euclidean_distanceg(
